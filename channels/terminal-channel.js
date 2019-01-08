49d9f3c85185eaae253229721f6457c40cfac464
Dennis Micky Jensen <root@mewm.org>
2019-01-08 22:35:51 +0100

    Mention works correctly

--- channels/terminal-channel.js
+++ channels/terminal-channel.js
@@ -29,11 +29,23 @@ function textRelay(client, message)
         return;
     }
 
-    const intendedMessage = explodedMessage.slice(2).join(' ');
+    let intendedMessage = explodedMessage.slice(2).join(' ');
     // Check for invalid message
     if(intendedMessage.length === 0) {
         console.log('Invalid message');
         return;
+    }
+
+    // Check if username is written after channel
+    const perhapsUser = explodedMessage.slice(2,3).toString();
+    if(perhapsUser.length > 0 && perhapsUser.substr(0,1) === '@') {
+        // Username detected, find it
+        const mentionedUser = perhapsUser.substr(1, perhapsUser.length);
+        const mentionedUserId = message.guild.members.find(x => x.nickname === mentionedUser);
+        if(mentionedUserId) {
+            // Replace mentionedUser in intendedMessage with their ID
+            intendedMessage = intendedMessage.replace('@' + mentionedUser, '<@' + mentionedUserId.id + '>');
+        }
     }
 
     // Relay the message
