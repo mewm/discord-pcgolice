d914773a7d171ab8d9f86b324938f1d0f4df30c6
Dennis Micky Jensen <root@mewm.org>
2019-01-08 21:42:22 +0100

    Validation on message and added role check when using text relay

--- channels/terminal-channel.js
+++ channels/terminal-channel.js
@@ -9,12 +9,38 @@ function run(client, message)
 
 function textRelay(client, message) 
 {
+    if(!message.member.roles.some(r=>["Server Moderator"].includes(r.name)) ) {
+        message.channel.send('Sorry, only Server Moderators can use this feature');
+        return;
+    }
+
     const explodedMessage = message.content.split(' ');
+
+    if(explodedMessage.length <= 1) {
+        console.log('Invalid number of arguments');
+        return;
+    }
+
     const channelId = explodedMessage[1].replace(/\D/g,'');
+
+    // Check for invalid channel mention
+    if(channelId.length === 0) {
+        console.log('Invalid channel');
+        return;
+    }
+
     const intendedMessage = explodedMessage.slice(2).join(' ');
+    // Check for invalid message
+    if(intendedMessage.length === 0) {
+        console.log('Invalid message');
+        return;
+    }
+
+    // Relay the message
     const channel = client.channels.get(channelId);
     channel.send(intendedMessage);
-    // console.log(`Channel: ${channelId} - Message: ${intendedMessage}`);
+
+    // Log in logchannel
     logchannel = client.channels.find(x => x.name === config.log_channel);
     logchannel.send(`${message.author.toString()} just relayed following in ${channel.name}: ${intendedMessage}`);
 }
